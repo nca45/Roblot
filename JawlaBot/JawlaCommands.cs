9181bafb19b71d91dae4df1a3c69a4ee2829890f
nathanacheung <nathanacheung@hotmail.com>
2018-05-05 21:15:29 -0700

    Initial Commit

---
+++ JawlaBot/JawlaCommands.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Interactivity;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace JawlaBot
+{
+    class JawlaCommands
+    {
+        [Command("test")]
+        public async Task TestCommand(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync("This test is successful!");
+        }
+
+        [Command("hooyah")]
+        public async Task HooyahAsync(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivityModule();
+            var text = "test";
+            await ctx.RespondAsync("Respond with 'test' to recieve a reaction!");
+
+            var msg = await interactivity.WaitForMessageAsync(xm => xm.Content.Contains(text), TimeSpan.FromSeconds(15));
+            if(msg != null)
+            {
+                await ctx.RespondAsync("you typed 'test!");
+            }
+            else
+            {
+                await ctx.RespondAsync("Nobody typed 'test' :( I sad");
+            }
+        }
+        [Command("waitfortyping"), Description("Waits for a typing indicator.")]
+        public async Task WaitForTyping(CommandContext ctx)
+        {
+            // first retrieve the interactivity module from the client
+            var interactivity = ctx.Client.GetInteractivityModule();
+
+            // then wait for author's typing
+            await ctx.RespondAsync("type something");
+            var chn = await interactivity.WaitForTypingChannelAsync(ctx.User, TimeSpan.FromSeconds(60));
+            if (chn != null)
+            {
+                // got 'em
+                await ctx.TriggerTypingAsync();
+                await ctx.RespondAsync($"{ctx.User.Mention}, you typed in {chn.Channel.Mention}!");
+            }
+            else
+            {
+                await ctx.RespondAsync("*yawn*");
+            }
+        }
+
+        [Command("poll"), Description("Run a poll with reactions.")]
+        public async Task Poll(CommandContext ctx, [Description("How long should the poll last.")] TimeSpan duration, [Description("What options should people have.")] params DiscordEmoji[] options)
+        {
+            // first retrieve the interactivity module from the client
+            var interactivity = ctx.Client.GetInteractivityModule();
+            var poll_options = options.Select(xe => xe.ToString());
+
+            // then let's present the poll
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Poll time!",
+                Description = string.Join(" ", poll_options)
+            };
+            var msg = await ctx.RespondAsync(embed: embed);
+
+            // add the options as reactions
+            for (var i = 0; i < options.Length; i++)
+                await msg.CreateReactionAsync(options[i]);
+
+            // collect and filter responses
+            var poll_result = await interactivity.CollectReactionsAsync(msg, duration);
+            var results = poll_result.Reactions.Where(xkvp => options.Contains(xkvp.Key))
+                .Select(xkvp => $"{xkvp.Key}: {xkvp.Value}");
+
+            // and finally post the results
+            await ctx.RespondAsync(string.Join("\n", results));
+        }
+    }
+}
---
+++ JawlaBot/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using Newtonsoft.Json;
+
+namespace JawlaBot
+{
+    class Program
+    {
+        static DiscordClient discord;
+
+        static CommandsNextModule commands;
+
+        static InteractivityModule interactivity;
+
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] arg)
+        {
+
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = cfgjson.Token,
+                TokenType = TokenType.Bot,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            });
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = cfgjson.CommandPrefix,
+                EnableDms = false
+            });
+
+            commands.RegisterCommands<JawlaCommands>();
+
+            //TODO: Add interactivity module and configuration
+
+            interactivity = discord.UseInteractivity(new InteractivityConfiguration
+            {
+                PaginationBehaviour = TimeoutBehaviour.Delete,
+                PaginationTimeout = TimeSpan.FromMinutes(5),
+                Timeout = TimeSpan.FromMinutes(5)
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().EndsWith("xd")) 
+                {
+                    await e.Message.RespondAsync("😠 There will be no 'xd's on my channel.");
+                }
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+    // this structure will hold data from config.json
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("prefix")]
+        public string CommandPrefix { get; private set; }
+    }
+}
