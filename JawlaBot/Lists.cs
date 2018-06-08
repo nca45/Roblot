86ab0f30d5df083f695ee4d108a12e3f351ab9f5
nathanacheung <nathanacheung@hotmail.com>
2018-06-07 17:35:58 -0700

    Dynamic retrieval for list text files

--- JawlaBot/Lists.cs
+++ JawlaBot/Lists.cs
@@ -10,21 +10,21 @@ namespace JawlaBot
 
         public static string PUBGDrops(string map)
         {
-            var mapFile = File.ReadAllLines($@"C:\Users\Naysan\source\repos\JawlaBot\JawlaBot\{map}Drops.txt");
+            var mapFile = File.ReadAllLines(Program.currentDirectory + $@"\textfiles\{map}Drops.txt");
             Random rnd = new Random();          
             return mapFile[rnd.Next(mapFile.Length)];
         }
 
         public static string Restaurants()
         {
-            var list = File.ReadAllLines($@"C:\Users\Naysan\source\repos\JawlaBot\JawlaBot\Restaurants.txt");
+            var list = File.ReadAllLines(Program.currentDirectory + @"\textfiles\Restaurants.txt");
             Random rnd = new Random();
             return list[rnd.Next(list.Length)];
         }
 
         public static string[] ListRestaurants()
         {
-            return File.ReadAllLines($@"C:\Users\Naysan\source\repos\JawlaBot\JawlaBot\Restaurants.txt");
+            return File.ReadAllLines(Program.currentDirectory + @"\textfiles\Restaurants.txt");
         }
 
         //add restaurant? Depends on how often we try out new places to eat.
