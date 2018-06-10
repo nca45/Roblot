2655aca8bc7656e8b4868f2c30e6ba5970057b0e
nathanacheung <nathanacheung@hotmail.com>
2018-06-10 12:13:22 -0700

    update

--- JawlaBot/JawlaCommands.cs
+++ JawlaBot/JawlaCommands.cs
@@ -386,7 +386,7 @@ namespace JawlaBot
 
                 var psi = new ProcessStartInfo
                 {
-                    FileName = JawlaBot.currentDirectory + @"\ffmpeg.exe",
+                    FileName = "ffmpeg",
                     Arguments = $@"-i ""{file}"" -ac 2 -f s16le -ar 48000 pipe:1 ",
                     RedirectStandardOutput = true,
                     UseShellExecute = false
@@ -420,7 +420,7 @@ namespace JawlaBot
         public async Task LongestYeahBoi(CommandContext ctx)
         {
 
-            await StreamAudio(ctx, JawlaBot.currentDirectory + @"\yeahboi.ogg", "yeahboi");
+            await StreamAudio(ctx, JawlaBot.currentDirectory + "/yeahboi.ogg", "yeahboi");
         }
 
         [Command("stop")]
@@ -458,7 +458,7 @@ namespace JawlaBot
         private string GetRandomFile(string directory) //grabs random file from the directory
         {
             Random rnd = new Random();
-            var fileName = System.IO.Directory.GetFiles(JawlaBot.currentDirectory + $@"\{directory}", "*ogg");
+            var fileName = System.IO.Directory.GetFiles(JawlaBot.currentDirectory + $"/{directory}", "*ogg");
             return fileName[rnd.Next(0, fileName.Length)];
         }
 
